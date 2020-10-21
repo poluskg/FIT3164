a50eb631fe0d101db2424e15b4b42af62cefca8a
Katie Polus <kpol0001@student.monash.edu>
2020-10-21 18:54:57 +1100

    Front end edits

--- ui_helper.R
+++ ui_helper.R
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-require(shiny)
-require(shinydashboard)
-require(GGally)
-require(psych)
-require(plotly)
-require(hash)
-
-source("overview_ui.R")
-source("dataOverview_ui.R")
-source("userInputs_ui.R")
-source("predictiveModel_ui.R")
-source("references_ui.R")
-
-#Load in predictive model
-load(file = "z.Rdata")
-load(file = "baggingmodel.rda")
-
-#Load data
-data <- read.csv("Z-Alizadeh_sani_dataset.csv", header = TRUE)
-=======
 require(shiny)
 require(shinydashboard)
 require(GGally)
@@ -31,6 +10,7 @@ require(tidyverse)
 source("overview_ui.R")
 source("dataOverview_ui.R")
 source("userInputs_ui.R")
+source("modellingProcess_ui.R")
 source("predictiveModel_ui.R")
 source("references_ui.R")
 
@@ -40,4 +20,4 @@ load(file = "baggingmodel.rda")
 
 #Load data
 data <- read.csv("Z-Alizadeh_sani_dataset.csv", header = TRUE)
->>>>>>> ac6db7dd803f009bdac3c194dc2cdfc3bd7be5f9
+
