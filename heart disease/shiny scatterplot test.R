2d87eb918ca8edd78316ddd3452ec5ed43919b84
juliapaterson <69191581+juliapaterson@users.noreply.github.com>
2020-10-26 17:07:05 +1100

    Update shiny scatterplot test.R
    add file.exists

--- heart disease/shiny scatterplot test.R
+++ heart disease/shiny scatterplot test.R
@@ -1,8 +1,12 @@
 library(ggplot2)
 library(ggpubr)
+library(testthat)
 
+#check shiny file where scatterplot exists for robustness
+file.exists('dataOverview_ui.R')
+
+#manually create scatterplots for variable combinations to confirm visually
 attach(za)
-
 ggscatter(za, x = "Lymph", y = "WBC", add = 'reg.line', 
           conf.int = TRUE, cor.coef = TRUE, cor.methods = 'pearson',
           xlab = 'X Var', ylab = 'Y Var')
